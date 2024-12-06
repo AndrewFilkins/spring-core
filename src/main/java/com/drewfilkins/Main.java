9abdefdffdbe4d194743a956e0776db20afe24a4
AndrewFilkins <47746866+AndrewFilkins@users.noreply.github.com>
2024-12-06 22:50:35 +0300

    Deleted PropertySource annotation from main class

--- src/main/java/com/drewfilkins/Main.java
+++ src/main/java/com/drewfilkins/Main.java
@@ -4,7 +4,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:/src/main/resources/application.properties")
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.drewfilkins");
